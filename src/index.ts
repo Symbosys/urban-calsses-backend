7eed98fb76bd02823ff2251cf77085eeb43b341e
Symbosys <symbosystech@gmail.com>
2026-04-17 13:27:45 +0530

    change some code

--- src/index.ts
+++ src/index.ts
@@ -28,13 +28,15 @@ const app = express();
 const PORT = process.env.PORT || 4000;
 
 // Middlewares
-app.use(cors({
-  origin: [
-    "http://localhost:5173",
-    "https://urban-calsses-website.vercel.app/"
-  ],
-  credentials: true
-}));
+// app.use(cors({
+//   origin: [
+//     "http://localhost:5173",
+//     "https://urban-calsses-website.vercel.app/"
+//   ],
+//   credentials: true
+// }));
+
+app.options("*", cors());
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
