5415c1465170704871a9592dea3e7ff5282f5dbb
Symbosys <symbosystech@gmail.com>
2026-04-17 15:14:58 +0530

    change the cors

--- src/index.ts
+++ src/index.ts
@@ -34,20 +34,6 @@ app.use((req, res, next) => {
 
 
 
-// app.use(cors({
-//   origin: (origin, callback) => {
-//     const allowed = [
-//       "http://localhost:5173",
-//       "http://localhost:3000",
-//       "http://127.0.0.1:5173",
-//       "http://127.0.0.1:3000",
-//       "https://urbanclasses.in",
-//       "https://www.urbanclasses.in",
-//       "https://urban-calsses-website.vercel.app",
-//       "https://urban-calsses-admin.vercel.app",
-//       "https://unbarclasses.in"
-//     ];
-
 app.use(cors({
   origin: [
     "http://localhost:5173",
@@ -57,21 +43,14 @@ app.use(cors({
     "https://urbanclasses.in",
     "https://www.urbanclasses.in",
     "https://urban-calsses-website.vercel.app",
-    "https://urban-calsses-admin.vercel.app"
+    "https://urban-calsses-admin.vercel.app",
+    "https://unbarclasses.in"
   ],
-  credentials: true
+  credentials: true,
+  optionsSuccessStatus: 200
 }));
-//     if (!origin || allowed.includes(origin)) {
-//       callback(null, true);
-//     } else {
-//       callback(null, false);
-//     }
-//   },
-//   credentials: true,
-//   optionsSuccessStatus: 200
-// }));
 
-app.options(/(.*)/, cors()); // Explicitly handle preflight for all routes using regex to avoid path-to-regexp error
+app.options(/(.*)/, cors()); // Use regex instead of '*' to avoid routing errors in modern environments
 
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
